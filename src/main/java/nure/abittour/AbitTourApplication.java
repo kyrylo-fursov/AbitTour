b76c86e3ecee6d213fcc1cf95d5398b493042802
Kyrylo Fursov <85443017+kyrylo-fursov@users.noreply.github.com>
2024-04-28 22:18:27 +0300

    Initial Commit

---
+++ src/main/java/nure/abittour/AbitTourApplication.java
@@ -0,0 +1,11 @@
+package nure.abittour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbitTourApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AbitTourApplication.class, args);
+    }
+}
---
+++ src/test/java/nure/abittour/AbcVstupApplicationTests.java
@@ -0,0 +1,13 @@
+package nure.abittour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcVstupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
