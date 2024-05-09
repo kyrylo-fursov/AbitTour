8ad3238e7a94dffc4ebbc1519b7b55f5aa4af659
stChous <stratienkosaska@gmail.com>
2024-05-09 16:19:09 +0300

    fix speciality + fix json

--- src/main/java/nure/abittour/model/Speciality.java
+++ src/main/java/nure/abittour/model/Speciality.java
@@ -1,33 +1,17 @@
 package nure.abittour.model;
 
-import jakarta.persistence.*;
-import nure.abittour.model.enums.EducationalLevel;
-import nure.abittour.model.enums.EnrolmentBase;
-import nure.abittour.model.enums.FormOfEducation;
 
-import java.util.List;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 
 @Entity
 public class Speciality extends BaseEntity {
-    @Column(nullable = false)
-    String competitionIdentifier;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    EducationalLevel degreeOfHigherEducation;
-
-    @Enumerated(EnumType.STRING)
-    @ElementCollection
-    @Column(nullable = false)
-    List<EnrolmentBase> enrolmentBase;
-
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
-    FormOfEducation formOfEducation;
-
     @Column(nullable = false)
     String code;
 
     @Column(nullable = false)
     String name;
+
+    @Column
+    String specialization;
 }
