43693eaa861db72af9d15427d7cf1ad058d4f45e
stChous <stratienkosaska@gmail.com>
2024-05-21 16:27:05 +0300

    crud

--- src/main/java/nure/abittour/repository/SubjectRepository.java
+++
@@ -1,7 +0,0 @@
-package nure.abittour.repository;
-
-import nure.abittour.model.Subject;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectRepository extends JpaRepository<Subject, Long> {
-}
