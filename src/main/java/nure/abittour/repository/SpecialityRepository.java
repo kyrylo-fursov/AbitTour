690b67fa7e27f802e153aa1b7a76421587d84c9d
stChous <stratienkosaska@gmail.com>
2024-06-10 23:25:33 +0300

    Работающий контроллер апликух и куча неработающего хлама связанного с кэфами нмт

--- src/main/java/nure/abittour/repository/SpecialityRepository.java
+++ src/main/java/nure/abittour/repository/SpecialityRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import nure.abittour.model.Speciality;
 
 public interface SpecialityRepository extends JpaRepository<Speciality, Long> {
+    Speciality findByCode(String code);
+    Speciality findAllByCode(String code);
 }
--- src/main/java/nure/abittour/service/DatabaseInitializerService.java
+++ src/main/java/nure/abittour/service/DatabaseInitializerService.java
@@ -1,9 +1,13 @@
 package nure.abittour.service;
 
 import nure.abittour.model.Region;
+import nure.abittour.model.SubjectCoef;
+import nure.abittour.model.enums.Subject;
 import org.springframework.stereotype.Service;
 import jakarta.annotation.PostConstruct;
 import java.io.FileReader;
+import java.util.List;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -11,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import nure.abittour.model.Speciality;
 import nure.abittour.repository.SpecialityRepository;
 import nure.abittour.repository.RegionRepository;
+import nure.abittour.repository.SubjectCoefRepository;
 
 @Service
 public class DatabaseInitializerService {
@@ -19,6 +24,8 @@ public class DatabaseInitializerService {
     private SpecialityRepository specialityRepository;
     @Autowired
     private RegionRepository regionRepository;
+    @Autowired
+    private SubjectCoefRepository subjectCoefRepository;
 
     @PostConstruct
     public void initDb() {
@@ -57,5 +64,43 @@ public class DatabaseInitializerService {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+//        parser = new JSONParser();
+//        try {
+//            JSONArray jsonArray = (JSONArray) parser.parse(new FileReader("src/main/resources/subject_coefs.json"));
+//
+//            for (Object obj : jsonArray) {
+//                JSONObject jsonObject = (JSONObject) obj;
+//
+//                // Найти специальность по коду из JSON-файла
+//                String code = (String) jsonObject.get("code");
+//                Speciality speciality = specialityRepository.findByCode(code);
+//
+//                // Если специальность найдена, сохранить коэффициенты предметов
+//                if (speciality != null) {
+//                    JSONObject subjects = (JSONObject) jsonObject.get("subjects");
+//                    for (Object subjectKey : subjects.keySet()) {
+//                        String subjectName = (String) subjectKey;
+//                        Double coefficient = (Double) subjects.get(subjectName);
+//
+//                        // Создать объект SubjectCoef и установить связь с найденной специальностью
+//                        SubjectCoef subjectCoef = new SubjectCoef();
+//                        subjectCoef.setSpeciality(speciality);
+//                        subjectCoef.setSubject(Subject.valueOf(subjectName));
+//                        subjectCoef.setCoefficient(coefficient);
+//
+//                        // Сохранить объект в базе данных
+//                        subjectCoefRepository.save(subjectCoef);
+//                    }
+//                } else {
+//                    System.out.println("Специальность с кодом " + code + " не найдена.");
+//                }
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+
+
     }
+
 }
