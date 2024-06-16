63f01581c7fa06fde7f0b0356ad12bb5e403d861
Maksym Panasenko <maxpanasenkohello@gmail.com>
2024-06-16 18:45:50 +0300

    token change

--- abittour_frontend/src/App.js
+++ abittour_frontend/src/App.js
@@ -8,7 +8,7 @@ import { Header } from "./components/Header";
 // TODO: fix offer right column positioning
 
 export const jwt_token =
-  "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0dXNlckBnbWFpbC5jb20iLCJpYXQiOjE3MTg1Mzg4NzIsImV4cCI6MTcxODU3NDg3Mn0.dZfCkA8CtOzGywiZQT075LVLOap5zLGDdLJVdgrYDSA";
+  "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0dXNlckBnbWFpbC5jb20iLCJpYXQiOjE3MTg0NTQyNDcsImV4cCI6NDg3MjA1NDI0N30.sSKjAW2IJRjKJLc3-D4ISI3lx2hl-xdyo-WhV1Y4dGo";
 
 function App() {
   return (
